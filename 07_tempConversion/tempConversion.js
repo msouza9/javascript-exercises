7a13858e552226628d2b97abdd7d7cbaf33a9697
Michael <michaeljsouza9@gmail.com>
2022-08-14 16:58:37 -0700

    Finish exercise 07!
    EZ PZ

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let celcius = (temp - 32 ) * .5556;
+  rounded = Math.round(celcius * 10) / 10;
 
+  return rounded;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let farenheit = (temp * 1.8) + 32;
+  rounded = Math.round(farenheit * 10) / 10;
 
+  return rounded;
 };
 
 // Do not edit below this line
