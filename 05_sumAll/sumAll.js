0b53aee4827cd6bd67e6a1c8ff42cc7ef0a26df1
Michael <michaeljsouza9@gmail.com>
2022-08-13 20:56:20 -0700

    Finish exercise 05 SumAll
    Started exercise 06 leap year

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const sumList = [];
+    let sum = 0;
+    let bNum;
+    let tNum;
 
-};
+    if(num1 !== parseInt(num1) || num2 !== parseInt(num2)){
+        return 'ERROR';
+    }
+    else if(num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    else if(num1 > num2){
+       tNum = num1;
+       bNum = num2; 
+    }
+    else if (num1 < num2){
+        tNum = num2;
+        bNum = num1;
+    }
+    else {
+        return 'ERROR';
+    }
+
+    for (let i = bNum; i <= tNum; i++) {
+        sumList.push(i);
+    }
+
+    sumList.forEach((item) => {
+        sum += item;
+    });
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
