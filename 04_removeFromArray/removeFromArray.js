08fac5654a9d2d56e829087a12206504c8cc902a
Michael <michaeljsouza9@gmail.com>
2022-08-13 18:57:39 -0700

    Finish up to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...valuesToCheck) {    
+    const oldArray = valuesToCheck[0]; /**copies over original array at [0] element */
+    const newArray = []; /**this will be what we return */
+    
+    oldArray.forEach((item) => { /**this is a shorthand "for" loop to go thru each item in the orig array */
+        if(!valuesToCheck.includes(item)){ /**checks if orig array elements are in removal arguments passed into function */
+            newArray.push(item); /**creates new array without excluded parameters  */
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
