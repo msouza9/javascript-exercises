ae53ec4b58fffdac9ddcb9b78163beb879a55147
Michael <michaeljsouza9@gmail.com>
2022-08-24 23:16:39 -0700

    Finish palindromes exercise 09!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let backward = [];
+    let scrubbed = word.replace(/[!., ]/g,"");
+    let lowerScrub = scrubbed.toLowerCase();
+    
+    for(i=(lowerScrub.length-1); i>=0; i--){
+        backward.push(lowerScrub[i]);
+    }
 
+    backward = backward.join('');
+
+    if(backward === lowerScrub){
+        return true;
+    }
+    else {
+        console.log(backward);
+        return false;
+    }
 };
 
 // Do not edit below this line
