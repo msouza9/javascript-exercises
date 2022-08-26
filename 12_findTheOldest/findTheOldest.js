013151b52c578bc1959b6352a605c09d9db5cd74
Michael <michaeljsouza9@gmail.com>
2022-08-26 13:01:55 -0700

    Finish exercise 12!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {  
+    const ordered = array.sort((a, b) => getAge(a.yearOfBirth, a.yearOfDeath) < getAge(b.yearOfBirth, b.yearOfDeath) ? 1 : -1);
 
+    // console.log(ordered);
+    return ordered[Object.keys(ordered)[0]];
+};
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    else 
+    console.log(death - birth);
+    return death - birth;
 };
 
 // Do not edit below this line
